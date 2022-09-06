8fb5cafa53d902d46100bc616bf29894d2a63d71
hopeforkoami <hopeforkoami@gmail.com>
2022-09-06 18:36:31 +0000

    linter errors correction
    linter errors correction

---
+++ js/portofolio.js
@@ -0,0 +1,18 @@
+const menuItem = document.querySelector('.navbar_menu_content');
+    const closeMenu = document.querySelector('.close_button');
+    const openMenu = document.querySelector('.navbar_menu');
+    const menu = document.querySelector('.navbar_menu_content');
+    
+
+    function show() {
+      menuItem.classList.toggle("visible_nav_menu" );
+      menuItem.classList.remove("hidden");
+    }
+
+    function close() {
+      menuItem.classList.remove("visible_nav_menu" );
+      menuItem.classList.toggle("hidden");
+    }
+    openMenu.addEventListener('click', show);
+    closeMenu.addEventListener('click', close);
+    menu.addEventListener('click', close);
