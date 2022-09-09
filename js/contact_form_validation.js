b4e1d9ec8f74430e33181d93a4e24709ef5a7d04
hopeforkoami <hopeforkoami@gmail.com>
2022-09-09 21:09:11 +0000

    remarks correction
    taking into account all the various comments made by the reviewer

--- js/contact_form_validation.js
+++ js/contact_form_validation.js
@@ -5,10 +5,12 @@ const reg = /^[a-z0-9]+@[a-z0-9-]+\.[a-z0-9-.]+$/;
 function validate(e) {
   const emailvalue = email.value;
   if (emailvalue.match(reg)) {
+    error.style.display='none';
+    email.style.border = '1px solid #d0d9d4';
     return true;
   }
   error.style.display = 'block';
-  error.innerHTML = 'email should only contain low-case caracters';
+  error.innerHTML = 'Error : email should be in lower case';
   email.style.border = '2px solid red';
   e.preventDefault();
   return false;
